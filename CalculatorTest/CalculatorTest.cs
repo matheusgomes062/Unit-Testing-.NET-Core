3927eec504e278d07b896ed9abce3f0719bda3ca
Matheus Gomes <matheusgomes062@gmail.com>
2020-12-09 09:44:35 -0300

    initial commit

---
+++ CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace CalculatorTest
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
